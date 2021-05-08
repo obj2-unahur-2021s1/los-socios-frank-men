cf04a474112610c6630ae52d129b867d331f11b5
Daniel Mendez <rdmmendez@hotmail.com>
2021-05-08 17:12:55 -0300

    CLiente tiene la funcionalidad de calcula propina del promer punto

--- src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
@@ -1,4 +1,28 @@
 package ar.edu.unahur.obj2.socios
 
-class Cliente {
+class Cliente{
+    var plataEnBolsillo = 0.0
+    private lateinit var estado: Estado
+
+    fun setEstado(estado: Estado) {
+        this.estado = estado
+    }
+    fun calcularPropina(importePedido: Double) =
+        estado.calculaPropina(importePedido, plataEnBolsillo)
+}
+
+interface Estado{
+    fun calculaPropina(precio:Double, plataEnBolsillo: Double ): Double
+}
+class Enojado: Estado{
+    override fun calculaPropina(precio:Double, plataEnBolsillo: Double) = 0.0
+}
+class Feliz: Estado{
+    override fun calculaPropina(precio:Double, plataEnBolsillo: Double) = precio * 0.25
+}
+class Indiferente: Estado{
+    override fun calculaPropina(precio: Double, plataEnBolsillo: Double) = plataEnBolsillo
+}
+class Resfriado: Estado{
+    override fun calculaPropina(precio: Double, plataEnBolsillo: Double) = precio
 }
--- src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
@@ -1,8 +1,27 @@
 package ar.edu.unahur.obj2.socios
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 
 class ClienteTest : DescribeSpec({
   describe("Un/a cliente") {
+    val unCliente = Cliente()
+    unCliente.plataEnBolsillo = 15.5
+    it("enojado"){
+      unCliente.setEstado(Enojado())
+      unCliente.calcularPropina(1500.0).shouldBe(0.0)
+    }
+    it("feliz"){
+      unCliente.setEstado(Feliz())
+      unCliente.calcularPropina(1500.0).shouldBe(375.0)
+    }
+    it("indiferente"){
+      unCliente.setEstado(Indiferente())
+      unCliente.calcularPropina(1500.0).shouldBe(15.5)
+    }
+    it("resfriado/a"){
+      unCliente.setEstado(Resfriado())
+      unCliente.calcularPropina(1500.0).shouldBe(1500.0)
+    }
   }
 })
